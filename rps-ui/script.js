73d49e1e0b7a5ee562dee55ded6ef803076a5d86
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-14 05:18:45 -0500

    Completed UI functionality for RPS game. Includes buttons, choices display, outcome message, and score keeper

--- rps-ui/script.js
+++ rps-ui/script.js
@@ -14,18 +14,52 @@ function computerChoice () {
 // user and computer variables start as empty strings
 var user = "";
 var computer = "";
+var win = 0;
+var lose = 0;
+var draw = 0;
+
+// Displays result of game by creating a <div> to display outcome
+// <div> text will update on each button click
+const messageSpace = document.querySelector('#message');
+// This block displays the user and computer choices
+const choices =document.createElement('p');
+choices.textContent="";
+messageSpace.appendChild(choices);
+// This block displays the outcome of the round
+const outcome = document.createElement('p');
+outcome.textContent="";
+messageSpace.appendChild(outcome);
+
+
+// The blocks below are used to display W - L - D counter
+const winDisplay = document.createElement('p');
+winDisplay.textContent = `Wins: ${win}`;
+messageSpace.appendChild(winDisplay);
+
+const loseDisplay = document.createElement('p');
+loseDisplay.textContent = `Losses: ${lose}`;
+messageSpace.appendChild(loseDisplay);
+
+const drawDisplay = document.createElement('p');
+drawDisplay.textContent = `Draws: ${draw}`;
+messageSpace.appendChild(drawDisplay);
 
 // this function determines the win-loss-draw outcome based on user and computer choices
 function game(user, computer) {
-    console.log("You have chosen: ",user);
-    console.log("The computer has chosen: ", computer);
+    choices.textContent=`You have chosen ${user}; the computer chose ${computer}`;
 
     if ((user =="rock" && computer =="scissors") || (user =="paper" && computer =="rock") ||(user =="scissors" && computer =="paper")) {
-        console.log("You win!");
+        outcome.textContent="You win!";
+        ++win;
+        winDisplay.textContent = `Wins: ${win}`;
     } else if (user ==computer ) {
-        console.log("Draw!");
+        outcome.textContent="Draw!";
+        ++draw;
+        drawDisplay.textContent = `Draws: ${draw}`;
     } else if ((computer =="rock" && user =="scissors") || (computer =="paper" && user =="rock") ||(computer =="scissors" && user =="paper")) {
-        console.log("You lose!");
+        ++lose;
+        outcome.textContent="You lose!";
+        loseDisplay.textContent = `Losses: ${lose}`;
     }
 };
 

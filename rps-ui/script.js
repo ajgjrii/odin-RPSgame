6ee99a74c8a13ee7c8680ee94490ffbee00fd410
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-14 04:30:32 -0500

    Added comments to js to help with readability of code

--- rps-ui/script.js
+++ rps-ui/script.js
@@ -1,3 +1,4 @@
+// This function is used to randomly select a choice for the computer
 function computerChoice () {
     var rando = Math.floor(Math.random() * 3) + 1
     if (rando == 1) {
@@ -10,9 +11,11 @@ function computerChoice () {
     return computer;
 }
 
+// user and computer variables start as empty strings
 var user = "";
 var computer = "";
 
+// this function determines the win-loss-draw outcome based on user and computer choices
 function game(user, computer) {
     console.log("You have chosen: ",user);
     console.log("The computer has chosen: ", computer);
@@ -24,9 +27,9 @@ function game(user, computer) {
     } else if ((computer =="rock" && user =="scissors") || (computer =="paper" && user =="rock") ||(computer =="scissors" && user =="paper")) {
         console.log("You lose!");
     }
-
 };
 
+// sets var user to "rock" when rock button is clicked, then runs computer choice and game functions
 const rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
     var user="rock";
@@ -34,6 +37,7 @@ rock.addEventListener('click', () => {
     game(user, computer);
 });
 
+// sets var user to "paper" when paper button is clicked, then runs computer choice and game functions
 const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
     var user="paper";    
@@ -41,12 +45,10 @@ paper.addEventListener('click', () => {
     game(user, computer);
 });
 
+// sets var user to "scissors" when scissors button is clicked, then runs computer choice and game functions
 const scissors = document.querySelector('.scissors');
 scissors.addEventListener('click', () => {
     var user="scissors";    
     computerChoice();
     game(user, computer);   
 });
-
-
-

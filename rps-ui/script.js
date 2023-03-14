e55e20be294f664d9529803597712fc325a928a7
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-14 04:23:28 -0500

    Buttons display user and computer choices in console; winner displayed in console

--- rps-ui/script.js
+++ rps-ui/script.js
@@ -1,4 +1,4 @@
-function compChoice () {
+function computerChoice () {
     var rando = Math.floor(Math.random() * 3) + 1
     if (rando == 1) {
         computer = "rock";
@@ -13,35 +13,40 @@ function compChoice () {
 var user = "";
 var computer = "";
 
+function game(user, computer) {
+    console.log("You have chosen: ",user);
+    console.log("The computer has chosen: ", computer);
+
+    if ((user =="rock" && computer =="scissors") || (user =="paper" && computer =="rock") ||(user =="scissors" && computer =="paper")) {
+        console.log("You win!");
+    } else if (user ==computer ) {
+        console.log("Draw!");
+    } else if ((computer =="rock" && user =="scissors") || (computer =="paper" && user =="rock") ||(computer =="scissors" && user =="paper")) {
+        console.log("You lose!");
+    }
+
+};
 
 const rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
-    var user="rock";    
-    console.log("You have chosen: ",user);
-    compChoice();
-    console.log("The computer has chosen: ", computer)
+    var user="rock";
+    computerChoice();
+    game(user, computer);
 });
 
 const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
     var user="paper";    
-    console.log("You have chosen: ",user);
-    compChoice();
-    console.log("The computer has chosen: ", computer)
+    computerChoice();
+    game(user, computer);
 });
 
 const scissors = document.querySelector('.scissors');
 scissors.addEventListener('click', () => {
     var user="scissors";    
-    console.log("You have chosen: ",user);
-    compChoice();
-    console.log("The computer has chosen: ", computer)   
+    computerChoice();
+    game(user, computer);   
 });
 
 
 
-// const runGame = document.querySelector('.choice');
-// runGame.addEventListener('click', () => {
-//     console.log("You have chosen: ", user);
-//    console.log("The computer has chosen: ", computer);
-// });
